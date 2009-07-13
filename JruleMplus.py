ad90b3e46d1fc5ed94c0580f091d571dbeb612f0
daob <daniel.oberski@gmail.com>
2009-07-13 23:48:20 +0100

    plot responds to subselection

--- JruleMplus.py
+++ JruleMplus.py
@@ -2,6 +2,7 @@
 # vim: set fileencoding=utf-8 :
 
 import sys, os, re
+import copy
 import distributions
 
 import matplotlib   
@@ -43,11 +44,12 @@ class JruleGTK:
        GTK+ and Glade."""
 
     def __init__(self):
-	# Session vars, TODO: get from ini file
+        # Session vars, TODO: get from ini file
         self.filename = '' # The file to be read
         self.last_directory = 'test/'
 
         self.critical = False # just a caching var, False triggers calculations
+        self.plot = EmptyThing() # A hack to fool .reload()
 
         # Set the Glade file
         self.gladefile = "JruleMplus.glade"  
@@ -64,9 +66,9 @@ class JruleGTK:
         self.treeview = TreeView(self) # see ItemList class below
         self.messager = Messager(self) 
         self.combo_parameter = ComboBox('combo_parameter', self, ['BY', 'ON', 'WITH'])
-	self.combo_group = ComboBox('combo_group', self, []) # TODO: fill this
+        self.combo_group = ComboBox('combo_group', self, []) # TODO: fill in
         self.combo_decision = ComboBox('combo_decision', self, ['Misspecified', 
-	    'Not misspecified', 'Check EPC', 'Not enough information'])
+            'Not misspecified', 'Check EPC', 'Not enough information'])
         
         self.update_status() # show current file in status bar
         
@@ -77,7 +79,7 @@ class JruleGTK:
             'Not misspecified (EPC < delta)' : '#a8e8a1',
         } # default colors to give the background of the treeview
         self.use_colors = True # Whether to color bg of treeview
-        
+
         # connect widget signals to class functions
         self.window.connect("destroy", gtk.main_quit)
 
@@ -85,17 +87,29 @@ class JruleGTK:
                 "on_quit_mi_activate" :gtk.main_quit,
                 "on_about_mi_activate" : self.show_about,
                 "on_filechooser_file_set" : self.set_file,
-		"on_open_mi_activate" : self.set_file_from_menu,
+                "on_open_mi_activate" : self.set_file_from_menu,
                 "on_about_response" : self.about_response,
-                "on_delta_entry_changed" : self.reload,
-                "on_alpha_entry_changed" : self.reload,
-                "on_power_entry_changed" : self.reload,
+                "on_delta_entry_changed" : self.reload, # on out of tab
         }
         self.tree.signal_autoconnect(dic) 
         self.window.show()
-        self.plot = None # A hack to fool .reload()
         self.reload() # fill the tree if there is a file
         self.plot = JPlot(self)
+
+    def reset_filter(self):
+        "Reset the application wide filter on parameters"
+        self.filtered_parameters = range(len(self.parameters))
+        self.already_filtered = []
+
+    def get_parameters(self):
+        "Get the parameters from the indices"
+        return [self.parameters[ind] for ind in self.filtered_parameters]
+
+    def filter_param(self, index):
+        "Filter out parameter with <index>. Assumes reset filter has been called"
+	if not index in self.already_filtered:
+            self.already_filtered.append(index)
+            self.filtered_parameters.remove(index)
 
     def jpaste(self, number):
         """Utility function to write a floating point number to text.
@@ -103,21 +117,21 @@ class JruleGTK:
         return eval("\%1." + self.digits + "f") % float(number)
 
     def set_file_from_menu(self, menuitem):
-	'''What happens if user selects Open from menu'''
-	self.set_file(None, filename = self.filechooser.ask() )
+        '''What happens if user selects Open from menu'''
+        self.set_file(None, filename = self.filechooser.ask())
 
     def set_file(self, filechooser=None, filename=''):
         """Given the user's choice of file, save this data and reload the list."""
-	if filechooser: self.filename = filechooser.get_filename()
-	elif filename: self.filename = filename
-	else: sys.stderr.write('[108] An error occurred trying to open the file.\n')
+        if filechooser: self.filename = filechooser.get_filename()
+        elif filename: self.filename = filename
+        else: sys.stderr.write('[108] An error occurred trying to open the file.\n')
         sys.stderr.write("File chosen is %s.\n" % self.filename)
         self.update_status()
         if not self.reload():
             self.messager.display_message('A problem occurred trying to read ' + 
                 '%s as an Mplus output file.' % self.filename +
                 ' Please make sure you have selected the right file.')
-    
+
     def reload(self, *args):
         """Reload the list using the MplusOutput class. Might be used as a callback
            so has variable number of arguments."""
@@ -146,18 +160,17 @@ class JruleGTK:
         self.statusbar.push(context_id, msg)
 
     def show_about(self, about_mi):
-	'Show about box'
+        'Show about box'
         self.aboutbox.run() 
 
     def about_response(self, aboutbox, signal):
-	'Handle signals from about box'
+        'Handle signals from about box'
         if signal < 0: aboutbox.hide()
 
-
     def get_field_value(self, which):
-	'''The judgement rules can be changed by the user via fields in the 
-	   rules tab. This function retrieves a value from a particular field.
-	   The name of this field should be passed in `which`.'''
+        '''The judgement rules can be changed by the user via fields in the 
+           rules tab. This function retrieves a value from a particular field.
+           The name of this field should be passed in `which`.'''
         which_dict = {'alpha': (self.alpha_entry, valid_alpha),
             'power': (self.power_entry, valid_power),
             'delta': (self.delta_entry, valid_delta),
@@ -180,13 +193,13 @@ class JruleGTK:
             return float(value)
 
     def get_critical(self):
-	"Return critical value of 1df chi square test from field or cached value"
+        "Return critical value of 1df chi square test from field or cached value"
         if not self.critical: # not cached
-	    self.critical = distributions.qchisq(1, self.get_field_value('alpha'))
+            self.critical = distributions.qchisq(1, self.get_field_value('alpha'))
         return self.critical
 
     def error(self, err_string):
-	'Just a shorthand for:'
+        'Just a shorthand for:'
         self.messager.display_message(err_string)
 
 
@@ -206,18 +219,18 @@ class JPlot:
         self.axis.set_title('Misspecifications')
         try:
             # omit missing observations
-            parameters = [par for par in self.app.parameters if \
+            parameters = [par for par in self.app.get_parameters() if \
                         par.mi <99.0 and par.power < 1.0 and par.epc < 99.0]
-            mis = [par.mi for par in parameters]
-            names = [par.name for par in parameters]
-            powers = [par.power for par in parameters]
+            self.mis = [par.mi for par in parameters]
+            self.names = [par.name for par in parameters]
+            self.powers = [par.power for par in parameters]
+            self.ids = ["%s in group %s" % (par.name, par.group) for par in parameters]
 
-            self.axis.scatter( mis, powers,
+            self.axis.scatter( self.mis, self.powers,
                 c = [par.epc > self.app.get_field_value('delta') \
                     and self.imp_col or self.nim_col for par in parameters],
-		marker = 'o',
-                alpha = 0.8, linewidth=0, picker=10.0, vmin=self.imp_col,
-		vmax = self.nim_col
+                linewidth=0, picker=30.0, vmin=self.imp_col,
+                vmax = self.nim_col,
             )
             self.axis.autoscale_view(True) #tight
 
@@ -227,20 +240,30 @@ class JPlot:
                  color='#444444', linestyle='dashed')
         except AttributeError:
             pass
+        self.draw()
+
+    def pick_handler(self, event):
+        '''What happens if the user clicks a point in the plot'''
+        index = event.ind[0]
+        label = self.ids[index]
+        sys.stderr.write('ind is %s'% index)
+        sys.stderr.write('mousover %s\n' % label)
+        x, y = self.mis[index], self.powers[index]
+        self.axis.text(x, y, label)
+        self.draw()
+
+    def draw(self):
+        'Draw or re-draw the plot, possibly showing some labels'
+        try: self.graphview.remove(self.canvas)
+        except: pass
         self.canvas = FigureCanvasGTK(self.figure)
         self.canvas.mpl_connect('pick_event', self.pick_handler)
         self.canvas.show()
         self.graphview = self.app.tree.get_widget("vbox_plot")
         self.graphview.pack_start(self.canvas, True, True)
 
-    def pick_handler(self, event):
-	'''What happens if the user clicks a point in the plot'''
-        mouseevent = event.mouseevent
-        artist = event.artist
-        sys.stderr.write('mousover %s\n'%str(artist))
-        # nw do something with this...
-
     def reload(self):
+        'Completely redo the whole plot.'
         self.graphview.remove(self.canvas)
         self.__init__(self.app)
 
@@ -285,6 +308,7 @@ class TreeView:
             self.treemodelfilter.set_visible_func(visible_func, data=None)
         self.treemodelsort = gtk.TreeModelSort(self.treemodelfilter)
         self.treeview.set_model(self.treemodelsort)
+        self.application.plot.reload() # also redraw the plot
 
     def populate_tree(self):
         """Uses MplusOutput class linked to the parent application
@@ -301,17 +325,23 @@ class TreeView:
                         self.application)
                     self.application.parameters.append(parameter)
                     parameter.append_to_tree(self.treestore)
+        self.application.reset_filter() # reset parameter filter
 
     def filter(self, by, filter_text, filter_re):
         """Filters the parameter list by regular expression for one of the fields"""
         sys.stderr.write('Filtering.. by=%s; text=%s\n'%(by,filter_text))
+        self.application.reset_filter() # reset parameter filter
+	colnum = [name.lower() for name in self.column_names].index(by.lower())
         def visible_func(model, iter, user_data):
-            if not filter_text: return True #Don't even bother
-            colnum = [name.lower() for name in self.column_names].index(by.lower())
+            if not (filter_text and model.get_value(iter, colnum)): return True
             found = filter_re.search(str(model.get_value(iter, colnum)))
-            return found and True or False  # strange: does not work w/o True/F
+            if found: return True
+            else:   # let the app know this param should be filtered out
+                self.application.filter_param(self.treestore.get_path(iter)[0])
+                return False
 
         self.reload(visible_func)
+
 
 class ComboBox:
     """A class for the three combobox filters."""
@@ -376,7 +406,7 @@ class Parameter:
         """Show this parameter in a TreeStore"""
         # TODO: Give a different bg-color to different decisions
         # TODO: Give a different color to text describing groups
-	decision = self.get_decision()
+        decision = self.get_decision()
         treestore.append( None, (self.name, decision, str(self.group),
             jpaste(self.mi), jpaste(self.epc), 
             jpaste(self.power), jpaste(self.ncp), 
@@ -403,45 +433,49 @@ class Parameter:
 
         return decision
 
+
 class FileChooser:
     '''Handles opening files, using the GTK filechooser widget and dialog'''
     def __init__(self, app, default_dir = 'tests/'):
-	'''Set application, default directory and filters'''
-	self.app = app
-	self.default_dir = default_dir # so program can remember last session
-	self.widget =  app.tree.get_widget("filechooser")
+        '''Set application, default directory and filters'''
+        self.app = app
+        self.default_dir = default_dir # so program can remember last session
+        self.widget =  app.tree.get_widget("filechooser")
 
-	self.filters = [] # will all be added as filters
+        self.filters = [] # will all be added as filters
         filter = gtk.FileFilter()
         filter.set_name("Output files")
         filter.add_pattern("*.out") # the official/default Mplus output extension
         filter.add_pattern("*.txt") # I guess some people save from Notepad..
-	self.filters.append(filter)
+        self.filters.append(filter)
 
         filter = gtk.FileFilter()
         filter.set_name("All files")
         filter.add_pattern("*")
-	self.filters.append(filter)
+        self.filters.append(filter)
 
     def ask(self):
-	'''Ask user for a file and return the filename chosen'''
-	dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_OPEN,
-                                  buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,
-				      gtk.STOCK_OPEN,gtk.RESPONSE_OK))
-	for filter in self.filters: dialog.add_filter(filter)
-	dialog.set_current_folder(self.default_dir)
-	response = dialog.run()
+        '''Ask user for a file and return the filename chosen'''
+        dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_OPEN,
+                         buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,
+                         gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+        for filter in self.filters: dialog.add_filter(filter)
+        dialog.set_current_folder(self.default_dir)
+        response = dialog.run()
         if response == gtk.RESPONSE_OK: filename = dialog.get_filename()
         elif response == gtk.RESPONSE_CANCEL: filename = ''
         dialog.destroy()
-	return filename
+        return filename
 
-	
-	  
+
+class EmptyThing:
+    def __init__(self): pass
+    def reload(self): pass
+          
 
 if __name__ == "__main__":
-    #try: sys.stderr = file('logfile.txt', 'w')
-    #except: pass
+    try: sys.stderr = file('logfile.txt', 'w')
+    except: pass
     app = JruleGTK()
     gtk.main()
-    #sys.stderr.close()
+    sys.stderr.close()
